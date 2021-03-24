effe7ce82a7c83307069d420c744dd54f1337bfa
srinath-pandit <srinath@targetclose.com>
2021-03-24 16:49:01 +0530

    updated name

--- Module1-Nextjs/pages/index.js
+++ Module1-Nextjs/pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello Next World Srinath!</h1>
+      <h1>Hello Next World Srinath! srini</h1>
     </div>
   )
 }
